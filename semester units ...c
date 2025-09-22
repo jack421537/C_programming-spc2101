896b500aee4ef217482243ed4a697e5ec7100755
jack421537 <irisjalle@gmail.com>
2025-09-22 22:14:31 +0300

    Add files via upload

---
+++ semester units ...c
@@ -0,0 +1,25 @@
+ //simple C program (Hello world)
+ /*
+ Name: Jackson Mutinda 
+ Reg No:CT101/G/26534/25
+ Date: 18/09/2025
+ Description: semester units 
+ */
+ #include<stdio.h> // preprocessor directive scan (). printf()//main function 
+ //main function 
+int main(){
+ //display the semester units 
+ printf("semester units\n\n");
+ 
+ printf("1.mathematics for science\n");
+ printf("2.physics\n");
+ printf("3.analogue electronic\n");
+ printf("4.discrete Mathematics\n");
+ printf("5.introduction to programming and problem solving\n");
+ printf("6.fundamentals of ICT\n");
+ printf("7.communicaton skills\n");
+ printf("8.introduction to computer architecture\n");
+ 
+ printf("\nTotal units:8\n");
+    return 0;
+ }
